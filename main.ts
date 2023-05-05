854a7c1d8f68cc3b411b9bf447e1aa04d241e1e6
jmichalenko <50920207+jmichalenko@users.noreply.github.com>
2023-05-05 16:50:03 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,36 @@
+let buttonVal = 0
+let item = 0
+function button () {
+    buttonVal = pins.analogReadPin(AnalogPin.P2)
+    if (buttonVal < 256) {
+        item = 1
+    } else if (buttonVal < 597) {
+        item = 2
+    } else if (buttonVal < 725) {
+        item = 3
+    } else if (buttonVal < 793) {
+        item = 4
+    } else if (buttonVal < 836) {
+        item = 5
+    } else if (buttonVal < 938) {
+        item = 6
+    } else {
+        item = 0
+    }
+}
+basic.forever(function () {
+    button()
+    if (item) {
+        basic.showNumber(item)
+    } else if (pins.analogReadPin(AnalogPin.P0) < 400) {
+        basic.showString("-X")
+    } else if (pins.analogReadPin(AnalogPin.P0) > 600) {
+        basic.showString("+X")
+    } else if (pins.analogReadPin(AnalogPin.P1) < 400) {
+        basic.showString("-Y")
+    } else if (pins.analogReadPin(AnalogPin.P1) > 600) {
+        basic.showString("+Y")
+    } else {
+        basic.clearScreen()
+    }
+})
